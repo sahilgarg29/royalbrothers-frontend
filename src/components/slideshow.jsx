e4384ff10d6bfcc3d43d418192782e5c707bd1e4
Dasari Rajnarayana <drajnaryana@gmail.com>
2022-02-22 23:29:30 +0530

    updatedcode

---
+++ src/components/slideshow.jsx
@@ -0,0 +1,83 @@
+import React from "react"
+import "../components/slideshow.css"
+const colors = [{
+
+    image:"https://d36g7qg6pk2cm7.cloudfront.net/assets/icons/slide-one-8c7f76cbbc58a89e95e8fcc1ab03d3d4cf12ef680989755fcf01f3a725d5d775.jpg",
+     head:"We're socially celebrated",
+     para:"Our customer centric approach has got amazing reviews. Be it in any platform, you will see many users recommending our service "
+},{
+    image:"https://d36g7qg6pk2cm7.cloudfront.net/assets/icons/slide-two-2d63ed83b9f2b4003344ac868bb2a42d29efe841bb2c5894ac53bd6af85adb86.jpg",
+    head:"Ready to serve you in 40+ cites",
+    para:"Think of a city & we are mostaly there . The next time you plan on exploring a city, know that you are mean of transport is sorted with royal brothers. #BhaiHaiNa"
+},
+{
+    image:"https://d36g7qg6pk2cm7.cloudfront.net/assets/icons/slide-three-a8787ad27e9ea8ab39d164f233b0995c76d59c4a517c0313e11a4122d6309038.jpg",
+    head:"Bikes Maintained so good , you will feel it is brand new",
+    para:"We obsess on giving you the best possible experience. This is not only with well surviced bikes but also with a 24/7 support team available just a call away."
+}
+];
+const delay = 2500;
+export const Slideshow=()=>{
+  const [index, setIndex] = React.useState(0);
+  const timeoutRef = React.useRef(null);
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  React.useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
+  return (
+    <div className="slideshow">
+      <div
+        className="slideshowSlider"
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      >
+        {colors.map((e, index) => (
+           
+          <div
+            className="slide"
+            key={index} style={{background:  `url(${e.image}) center` }}>
+                 
+                <div className="slideDiv">
+                <h2>{e.head}</h2>
+                <p>{e.para}</p>
+                </div>
+             
+         
+          </div>
+        ))}
+      </div>
+
+      <div className="slideshowDots">
+
+        {colors.map((_, idx) => (
+          <div
+            key={idx}
+            className={`slideshowDot${index === idx ? " active" : ""}`}
+            onClick={() => {
+              setIndex(idx);
+            }}
+          ></div>
+        ))}
+      </div>
+      
+        
+    </div>
+  );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,138 @@
 import React from "react";
 import "./home.css";
-
+import {Slideshow} from "../../components/slideshow"
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Box from '@mui/material/Box';
 const Home = () => {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
   return <div>
-     <h1>Home Page</h1>
+      <div className="cashback">
+    
+      </div>
+    
+     <div className="vehicleinfo">
+       <div>
+       <img src="https://d36g7qg6pk2cm7.cloudfront.net/assets/redesign/feature_tag_sanitized-a37325c03fb50fd9b2783d574ba67c40cb7b8a90d5951a85f72095904943e1d6.png"></img>
+        <p>Sanitized Vehicles</p> 
+       </div>
+        <div>
+        <img src="https://d36g7qg6pk2cm7.cloudfront.net/assets/redesign/feature_tag_insurance-84ee18a118bee4cfc4f774e0dbdb5d24431af458eb583d5d1c5822801e388764.png"></img>
+        <p>Vehicle Insurance</p>
+        </div>
+         <div>
+         <img src="https://d36g7qg6pk2cm7.cloudfront.net/assets/redesign/feature_tag_assistance-ec66ebeeb0ebb01468fc1729165040e172820de83d5037cf47a73f4e19177d68.png"></img>
+        <p>24/7 Road Assitance</p>
+         </div>
+      <div>
+      <img src="https://d36g7qg6pk2cm7.cloudfront.net/assets/redesign/feature_tag_maintainence-4ac25e7eb9ad6f8b78ec52640a7310d8083a21823e1fb54ca9872076c2bc5f34.png"></img>
+        <p>Bike Maintance</p>
+      </div>
+        
+     </div>
+     <div className="heading">
+     <h2 style={{textAlign:"center",color:"#212121"}}>OUR FLEET</h2>
+     <div className="yellowbox"></div>
+     </div>
+     <div className="duration">
+       <p>Duration</p>
+       <Box sx={{ minWidth: 220,boxShadow:"0 4px 6px -1px rgb(0 0 0 / 10%), 0 2px 4px -1px rgb(0 0 0 / 6%);" }}>
+
+     
+       <FormControl fullWidth>
+  <InputLabel id="demo-simple-select-label">Hourly</InputLabel>
+  <Select
+    labelId="demo-simple-select-label"
+    id="demo-simple-select"
+    value={age}
+    label="Age"
+    onChange={handleChange}
+  >
+    <MenuItem value={1}>Hourly</MenuItem>
+    <MenuItem value={7}>7 days</MenuItem>
+    <MenuItem value={15}>15 days</MenuItem>
+    <MenuItem value={30}>1 Month</MenuItem>
+    <MenuItem value={90}>3 Months</MenuItem>
+  </Select>
+</FormControl>
+</Box>
+     </div>
+     
+     <div className="excontainer">
+  
+     <div className="exlusiveBikes">
+       <div>
+         <h4>Honda Activa 5G</h4>
+         <img src="https://d3vp2rl7047vsp.cloudfront.net/bike_models/images/000/000/233/medium/Honda_activa_5G.png?1532687566"></img>
+          <hr></hr>
+          <h4> ₹20/hour</h4>
+          <button className="exbutton">BOOK NOW</button>
+       </div>
+     </div>
+     <div className="exlusiveBikes">
+       <div>
+         <h4>Honda Activa 5G</h4>
+         <img src="https://d3vp2rl7047vsp.cloudfront.net/bike_models/images/000/000/255/medium/Suzuki-access-125-2019.png?1563529796"></img>
+          <hr></hr>
+          <h4> ₹20/hour</h4>
+          <button className="exbutton">BOOK NOW</button>
+       </div>
+     </div>
+     <div className="exlusiveBikes">
+       <div>
+         <h4>Honda Activa 5G</h4>
+         <img src="https://d3vp2rl7047vsp.cloudfront.net/bike_models/images/000/000/018/medium/TB_350-min_2.png?1519739776"></img>
+          <hr></hr>
+          <h4> ₹20/hour</h4>
+          <button className="exbutton">BOOK NOW</button>
+       </div>
+     </div>
+     <div className="exlusiveBikes">
+       <div>
+         <h4>Honda Activa 5G</h4>
+         <img src="https://d3vp2rl7047vsp.cloudfront.net/bike_models/images/000/000/018/medium/TB_350-min_2.png?1519739776"></img>
+          <hr></hr>
+          <h4> ₹20/hour</h4>
+          <button className="exbutton">BOOK NOW</button>
+       </div>
+     </div>
+     </div>
+    
+    
+     <div className="heading">
+     <h2 style={{textAlign:"center",color:"#212121"}}>WE BELIEVE IN EQUALITY</h2>
+     <div className="yellowbox"></div>
+     </div>
+     <Slideshow/>
+     <div className="heading">
+     <h2 style={{textAlign:"center",color:"#212121"}}>FEATURED IN</h2>
+     <div className="yellowbox"></div>
+     </div>
+    
+     <div className="newsImage">
+       <div>
+       <img  src="https://d36g7qg6pk2cm7.cloudfront.net/assets/icons/news2-b2db5fe5ecf6968d4e47cba7876d3ae8a959a5de65901b5a6292032987a38bdd.png" alt="" />
+       </div>
+       <div>
+       <img src="https://d36g7qg6pk2cm7.cloudfront.net/assets/icons/news3-2eb9e0398a565284ca3c41d685e32de985b43ce00541889f59541ee91cb28e61.png" alt="" />
+       </div>
+       <div>
+       <img src="https://d36g7qg6pk2cm7.cloudfront.net/assets/icons/news1-0a213d6dc03c6d68b278a11f210efe7cd727bf2f1fc34982b90b34d2d1c875da.png" alt="" />
+       </div>
+       <div>
+       <img src="https://d36g7qg6pk2cm7.cloudfront.net/assets/icons/money_control-0301bddd4ac901172098f7d0b590e8dbed35e7fa1064d2ced799f9cbc9185d66.png" alt="" />
+       </div>
+       
+     
+     </div>
+      
+   
   </div>
        
        
