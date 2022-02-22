cd18fdd4aacfa3519c719208f541839f59a31dba
Dasari Rajnarayana <drajnaryana@gmail.com>
2022-02-22 11:22:21 +0530

    firstcommit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./home.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return <div>
+      <h1>Home Page</h1>
+  </div>
+       
+       
+      
 };
 
 export default Home;
