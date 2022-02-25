abfae95a2fa81ca0de42e8eea4aa4566da27b799
abhimis12 <93533712+abhimis12@users.noreply.github.com>
2022-02-25 12:57:11 +0530

    sidebar done

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,82 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+
+import ListItemText from "@mui/material/ListItemText";
+import IconButton from "@mui/material/IconButton";
+import ClearIcon from "@mui/icons-material/Clear";
+
+export  const TemporaryDrawer = () => {
+  const [state, setState] = React.useState({
+    left: false
+  });
+
+   const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+   const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 300 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <IconButton
+        style={{ marginLeft: "250px", marginBottom: "50px" }}
+        color="error"
+      >
+        <ClearIcon />
+      </IconButton>
+
+      <List>
+        {[
+          "Traiff",
+          "Store",
+          "Blog",
+          "Indian Bike Routes",
+          "Partner With Us",
+          "About Us",
+          "Term And Condition",
+          "Bike Tour",
+          "Privacy Policies",
+          "FAQ",
+          "Reach  Us"
+        ].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </Box>
+  );
+
+  return (
+    <div style={{float:"left"}}>
+      {["left"].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
